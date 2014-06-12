3a740264dd423d5a2e068e712b5f612e928fb164
ChrisSherlin <rhymeswithmerlin@gmail.com>
2014-06-11 20:32:02 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+if (typeof project === "undefined") { project = {}; }
+
+(function(win, $, undefined){
+
+  var module = {};
+
+  module.eventHandlers = function() {
+
+  };
+
+  module.init = function() {
+    module.eventHandlers();
+  };
+
+  // Kickoff
+  $(document).ready(function() {
+
+  });
+
+})(window, jQuery);
